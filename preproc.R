dcbcba2870a1e04e2500a617a6087652bd6ee05d
dvbelyaev <dvbelyaev@yandex.ru>
2016-05-06 14:22:15 +0300

    Script for data preprocessing

---
+++ preproc.R
@@ -0,0 +1,19 @@
+# Source: https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data
+
+library(lubridate)
+library(dplyr)
+
+# Data loading and NA removing
+data_raw <- read.csv("./GlobalLandTemperatures/GlobalLandTemperaturesByCountry.csv")
+data_raw <- na.omit(data_raw)
+
+# Calculating average temperatire by country-year
+data_raw$Year <- year(as.Date(data_raw$dt, "%Y-%m-%d"))
+
+data <- data_raw %>%
+        group_by(Country, Year) %>%
+        summarise(avgTemp = mean(AverageTemperature))
+
+# Writing tidy data
+write.csv(data, "./GlobalLandTemperatures/TemperaturesByCountry.csv")
+
